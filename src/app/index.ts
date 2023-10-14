b55ad903be2af43afde38f1cb5cb676331dd3089
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-10-14 07:48:55 +0100

    chore: added morgan logger

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,16 +3,23 @@ import compression from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 import preventParameterPollution from 'hpp';
+import morgan from 'morgan';
 
 function App() {
 	const app = express();
 
 	// middlewares
+	app.set('trust proxy', true);
 	app.use(cors());
 	app.use(express.json({ limit: '50kb' }));
 	app.use(helmet());
 	app.use(preventParameterPollution());
 	app.use(compression());
+	app.use(morgan('dev'));
+
+	app.use((_, response) => {
+		response.send('hello');
+	});
 
 	return app;
 }
